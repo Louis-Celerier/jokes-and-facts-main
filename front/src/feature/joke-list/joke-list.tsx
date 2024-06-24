fa11f050c578a53121d39b35adc1ba6a9ab09636
Louis <43373395+Louis-Celerier@users.noreply.github.com>
2024-06-24 19:40:23 +0200

    Update joke-list.tsx

--- front/src/feature/joke-list/joke-list.tsx
+++ front/src/feature/joke-list/joke-list.tsx
@@ -38,14 +38,12 @@ export function SelectBar() {
 
 export function JokeList({ jType }: { jType: string }) { 
   const { loading, error, value } = useAsync(async () => {
-    console.log("Essaie " + jType);
     const response = await fetch(`http://localhost:3001/jokes/${jType}`);
     return await response.json();
   }, [jType]);
 
   if (loading) return <div>Loading jokes...</div>;
   if (error) return <div>Error loading jokes: {error.message}</div>;
-    console.log(jType);
 
   return value.map((joke, index) => (<li key={index}>{joke.setup} - {joke.punchline}</li>));
 }
